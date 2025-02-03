fd194d2d16d397a0fc2fecdbbad2a08f4e70d080
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-02-03 00:33:16 -0300

    WIP

--- backend/src/modules/stops/stops.service.ts
+++ backend/src/modules/stops/stops.service.ts
@@ -20,100 +20,107 @@ export class StopsService {
     });
   }
 
-  private fetchNextArrivals({
+  private async fetchNextArrivals({
     stopIdentifier,
     lineCode,
   }: NextArrivalsParams): Promise<NextArrivalsResponse> {
-    return new Promise(async (resolve, reject) => {
-      const url = this.configService.get<string>('WS_URL');
+    const url = this.configService.get<string>('WS_URL');
+    const config = this.getSoapConfig('RecuperarProximosArribos');
+    const soapRequest = this.buildSoapRequest(stopIdentifier, lineCode);
 
-      const config = {
-        headers: {
-          'Content-Type': 'application/soap+xml;charset=UTF-8',
-          SOAPAction:
-            this.configService.get<string>('WS_NAMESPACE') +
-            '/RecuperarProximosArribos',
-        },
+    try {
+      const response = await axios.post(url, soapRequest, config);
+      return this.parseSoapResponse(response.data);
+    } catch (error) {
+      return {
+        message: error.message ?? 'Error fetching next arrivals',
+        statusCode: 500,
       };
+    }
+  }
 
-      const soapRequest =
-        '<?xml version="1.0" encoding="utf-8"?>' +
-        '<soap12:Envelope xmlns:xsi="http://www.w3.org/XMLSchema-instance" xmlns:xsd="http://www.w3.org/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">' +
-        '  <soap12:Body>' +
-        '    <RecuperarProximosArribos xmlns="http://clsw.smartmovepro.net/">' +
-        '      <usuario>' +
-        this.configService.get<string>('WS_USER') +
-        '</usuario>' +
-        '      <clave>' +
-        this.configService.get<string>('WS_PASSWORD') +
-        '</clave>' +
-        '      <codigoLineaParada>' +
-        lineCode +
-        '</codigoLineaParada>' + // Modificar con el código de línea deseado
-        '      <identificadorParada>' +
-        stopIdentifier +
-        '</identificadorParada>' +
-        '      <codigoAplicacion>24</codigoAplicacion>' +
-        '      <localidad>CORRIENTES</localidad>' +
-        '      <isSublinea>false</isSublinea>' +
-        '      <isSoloAdaptados>false</isSoloAdaptados>' +
-        '    </RecuperarProximosArribos>' +
-        '  </soap12:Body>' +
-        '</soap12:Envelope>';
+  private getSoapConfig(action: string) {
+    return {
+      headers: {
+        'Content-Type': 'application/soap+xml;charset=UTF-8',
+        SOAPAction: `${this.configService.get<string>('WS_NAMESPACE')}/${action}`,
+      },
+    };
+  }
 
-      await axios
-        .post(url, soapRequest, config)
-        .then(async (response) => {
-          xml2js.parseString(response.data, (err, response) => {
-            if (err) {
-              reject('Error al parsear XML');
-              return;
-            }
+  private buildSoapRequest(stopIdentifier: string, lineCode: string): string {
+    return (
+      '<?xml version="1.0" encoding="utf-8"?>' +
+      '<soap12:Envelope xmlns:xsi="http://www.w3.org/XMLSchema-instance" xmlns:xsd="http://www.w3.org/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">' +
+      '  <soap12:Body>' +
+      '    <RecuperarProximosArribos xmlns="http://clsw.smartmovepro.net/">' +
+      '      <usuario>' +
+      this.configService.get<string>('WS_USER') +
+      '</usuario>' +
+      '      <clave>' +
+      this.configService.get<string>('WS_PASSWORD') +
+      '</clave>' +
+      '      <codigoLineaParada>' +
+      lineCode +
+      '</codigoLineaParada>' +
+      '      <identificadorParada>' +
+      stopIdentifier +
+      '</identificadorParada>' +
+      '      <codigoAplicacion>24</codigoAplicacion>' +
+      '      <localidad>CORRIENTES</localidad>' +
+      '      <isSublinea>false</isSublinea>' +
+      '      <isSoloAdaptados>false</isSoloAdaptados>' +
+      '    </RecuperarProximosArribos>' +
+      '  </soap12:Body>' +
+      '</soap12:Envelope>'
+    );
+  }
 
-            const arrivals = JSON.parse(
-              response['soap:Envelope']['soap:Body'][0][
-                'RecuperarProximosArribosResponse'
-              ][0]['RecuperarProximosArribosResult'][0],
-            );
+  private parseSoapResponse(data: any): Promise<NextArrivalsResponse> {
+    return new Promise((resolve, reject) => {
+      xml2js.parseString(data, (err, result) => {
+        if (err) {
+          reject('Error parsing XML');
+          return;
+        }
 
-            if(arrivals.CodigoEstado === -1){
-              resolve({
-                message: arrivals.MensajeEstado,
-                statusCode: 400,
-              });
-            }
-
-            resolve({
-              message: 'Next arrivals fetched successfully',
-              result: arrivals.arribos.map((arrival) => ({
-                line_description: arrival.DescripcionLinea,
-                flag_description: arrival.DescripcionBandera,
-                arrival: arrival.Arribo,
-                latitude: arrival.Latitud,
-                longitude: arrival.Longitud,
-                stop_latitude: arrival.LatitudParada,
-                stop_longitude: arrival.LongitudParada,
-                stop_line_code: arrival.CodigoLineaParada,
-                short_flag_description: arrival.DescripcionCortaBandera,
-                flag_sign_description: arrival.DescripcionCartelBandera,
-                is_adapted: arrival.EsAdaptado,
-                car_identifier: arrival.IdentificadorCoche,
-                driver_identifier: arrival.IdentificadorChofer,
-                schedule_deviation: arrival.DesvioHorario,
-                last_gps_date: arrival.UltimaFechaHoraGPS,
-                error_message: arrival.MensajeError,
-                position: `https://www.google.com/maps/search/?api=1&query=${arrival['Latitud']},${arrival['Longitud']}`,
-              })),
-              statusCode: 200,
-            });
-          });
-        })
-        .catch((error) =>
-          resolve({
-            message: error.message ?? 'Error fetching next arrivals',
-            statusCode: 500,
-          })
+        const arrivals = JSON.parse(
+          result['soap:Envelope']['soap:Body'][0][
+            'RecuperarProximosArribosResponse'
+          ][0]['RecuperarProximosArribosResult'][0],
         );
+
+        if (arrivals.CodigoEstado === -1) {
+          resolve({
+            message: arrivals.MensajeEstado,
+            statusCode: 400,
+          });
+        }
+
+        resolve({
+          message: 'Next arrivals fetched successfully',
+          result: arrivals.arribos.map((arrival) => ({
+            line_description: arrival.DescripcionLinea,
+            flag_description: arrival.DescripcionBandera,
+            arrival: arrival.Arribo,
+            latitude: arrival.Latitud,
+            longitude: arrival.Longitud,
+            stop_latitude: arrival.LatitudParada,
+            stop_longitude: arrival.LongitudParada,
+            stop_line_code: arrival.CodigoLineaParada,
+            short_flag_description: arrival.DescripcionCortaBandera,
+            flag_sign_description: arrival.DescripcionCartelBandera,
+            is_adapted: arrival.EsAdaptado,
+            car_identifier: arrival.IdentificadorCoche,
+            driver_identifier: arrival.IdentificadorChofer,
+            schedule_deviation: arrival.DesvioHorario,
+            last_gps_date: arrival.UltimaFechaHoraGPS,
+            error_message: arrival.MensajeError,
+            position: `https://www.google.com/maps/search/?api=1&query=${arrival['Latitud']},${arrival['Longitud']}`,
+          })),
+          statusCode: 200,
+        });
+      });
     });
   }
 }
