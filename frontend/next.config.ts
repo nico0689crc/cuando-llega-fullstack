c8f629f65b88f8d4005e0fc54d75a2d10e84d584
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-02-06 01:41:35 -0300

    WIP

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
   trailingSlash: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+        locale: false
+      }
+    ]
+  }
 };
 
 export default nextConfig;
