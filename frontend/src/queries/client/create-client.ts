961ad18e15680c50d6d705407e527c95217a1ecc
Nicolas Fernandez <62648191+nico0689crc@users.noreply.github.com>
2025-02-14 21:10:06 -0300

    WIP

--- frontend/src/queries/client/create-client.ts
+++ frontend/src/queries/client/create-client.ts
@@ -5,8 +5,6 @@ export const createClient = () => {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
     uri: process.env.NEXT_PUBLIC_GRAPHQL_PATH,
-    // you can disable result caching here if you want to
-    fetchOptions: { cache: 'no-store' },
   })
 
   return new ApolloClient({
